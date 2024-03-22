64320d753e72186814f8b657e38f2b29d1b993ac
parvez <parvezcoder786@gmail.com>
2024-03-23 01:12:00 +0500

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{
+        name: "firstNumber",
+        type: 'number',
+        message: 'Enter Frist Number',
+    },
+    {
+        name: 'secondNumber',
+        type: 'number',
+        message: 'Enter Second Number',
+    },
+    {
+        name: 'Operator',
+        type: 'list',
+        message: 'Select one Operation',
+        choices: ['add', 'sub', 'mul', 'div']
+    }
+]);
+if ("add" === answer.Operator) {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.Operator === 'sub') {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.Operator === 'mul') {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.Operator === 'div') {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("try Again");
+}
