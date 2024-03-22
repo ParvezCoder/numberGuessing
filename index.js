f2cef6271562d627e2529de287e6b7e39adbcb72
parvez <parvezcoder786@gmail.com>
2024-03-23 02:54:41 +0500

    Shabang updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-let computerNumber = Math.floor(Math.random() * 0 + 1);
+let computerNumber = Math.floor(Math.random() * 6 + 1);
 let answer = await inquirer.prompt([{
         name: "myChoise",
         type: "number",
         message: `${chalk.red(`Hamza Sir`)} if you want to ${chalk.green("win the Game")}, then Guess the accurate
- Number Between ${chalk.red("1 and 6  🤔🤔🤔")} Han Soch looo `,
+ Number Between ${chalk.blue("1 and 6  🤔🤔🤔")} Han Soch looo `,
     }]);
 if (answer.myChoise === computerNumber) {
     console.log(`you ${chalk.blue.bold("win the game")}😤😎😤😎, because you Guess  ${chalk.red(`${answer.myChoise}`)}   and Computer Number is ${chalk.red(`${computerNumber}`)}`);
